094e28789a32da04d291f238895439d56c2c5672
Bharath Kumar Maruboyini <97357441+Bharathram1256@users.noreply.github.com>
2023-04-29 18:50:22 +0530

    Update shoppp.py

--- shoppp.py
+++ shoppp.py
@@ -28,7 +28,7 @@
 
 print('''List of avialable books are given below\nIntroduction to Python Programming : Rs 499.00\nPython Libraries Cookbook : Rs. 855.00\nSelect 3 for Data Science in Python : Rs. 645.00\ngive input 0 if you don't want that book to purchase''')
 
-def library():
+def library():                                                                #for taking the user inputs
     a=int(input("enter no.of Introduction to Python Programming = "))
     b=int(input("enter no.of Python Libraries Cookbook costs = "))
     c=int(input("enter no.of  Data Science in Python costs = "))
@@ -40,7 +40,7 @@ total=library()
 
 
 
-def delivery_charges(total):
+def delivery_charges(total):                                                        #checking for the total if the user give all the inputs as 0's then ask another time to purchase 
     if total==0:
         a=int(input("do you want purchase any book\n for YES click 1 \n for NO click 0"))
         if a==0:
@@ -52,11 +52,43 @@ def delivery_charges(total):
         return 250
     return 250
 
-including_gst=1.12*total+delivery_charges(total)
+including_gst=1.12*total+delivery_charges(total)                #calculating the total amount including gst and delivery charges
 i=including_gst
 print("Amount only for Books total = {0}".format(total))
 print("Amount for delivery charges = 250.00")
 print("12% GST")
 print("total+12% GST+Rs.250.00 Delivery Charges=",i)
+
+
+
+
+
+
+
+
+# Question 2 - 
+
+# Description: Write a program in Python to print the number of unique letters in a string. Only new letters from the string should be counted and not duplicates.
+
+                                  
+
+# Input : string1 = "India"
+
+# Output : uniqueLetters = i,n,d,a
+
+
+# Input : string1 = "Dedication"
+
+# Output : uniqueLetters = d,e,i,c,a,t,o,n
+
+# solution
+
+a=input('string1 = ')                 # taking the input
+b=set(a)                              # forming a set from user input
+print( 'uniqueLetters = ',end= '')    
+print(*b, sep = ',')                  #printing the required output
+
+
+
 
 
